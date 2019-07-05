876eb9b78c19658eb32c138b240d16261b974e45
Alexander Whillas <whillas@gmail.com>
2019-07-06 09:58:26 +1200

    Example AWS and Dropbox setups

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';  // Composer autoloader
 
+// Read in secrets from environment
 if(file_exists(__DIR__.DIRECTORY_SEPARATOR.'.env'))
 {
     $dotenv = Dotenv\Dotenv::create(__DIR__);
@@ -14,27 +15,27 @@ define('GIZMO_WEBSITE_TITLE', 'WebGizmo Mark II!');
 define('DEBUG', true);
 
 $content = array(
-	// '/' => [  // Where its mounted
-	// 	'type' => 'local',  	// default to local FS
-	// 	'config' => [ 'prefix' => '/content' ]  // this is the default 
-	// ],
-	'/s3' => [
-		'type' => 's3',
-		'config' => [
-			'bucket' => 'gizmo-test',
-			'prefix' => '/',
-			'credentials' => [
-				'key'    => $_ENV['AWS_ACCESS_KEY'],
-				'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-			],
-			'region' => 'ap-southeast-2',
-			'version' => 'latest',
-		],
+	'/' => [  // Where its mounted
+		'type' => 'local',  	// default to local FS
+		'config' => [ 'prefix' => '/content' ]  // this is the default 
 	],
-	// '/dropbox' => [
+	// '/somthing' => [
+	// 	'type' => 's3',
+	// 	'config' => [
+	// 		'bucket' => 'gizmo-test',
+	// 		'prefix' => '/',
+	// 		'credentials' => [
+	// 			'key'    => $_ENV['AWS_ACCESS_KEY'],
+	// 			'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+	// 		],
+	// 		'region' => 'ap-southeast-2',
+	// 		'version' => 'latest',
+	// 	],
+	// ],
+	// '/somethingelse' => [
 	// 	'type' => 'dropbox',
 	// 	'config' => [
-	// 		'prefix' => 'notes',
+	// 		'prefix' => '',
 	// 		'authorizationToken' => $_ENV['DROPBOX_API_TOKEN'],
 	// 	]
 	// ],	
